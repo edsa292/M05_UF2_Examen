174c1bd00f152e37c9090770779d89bde31889a4
Edgar Sanchez <edsa292@vidalibarraquer.net>
2023-05-12 11:15:12 +0200

    Push mètode sumar

--- UF2_Examen/src/Suma.java
+++ UF2_Examen/src/Suma.java
@@ -1,4 +1,9 @@
 public class Suma {
     public int num1;
     public int num2;
+
+    // Mètode per sumar dos números i retornar el resultat
+    public int sumar() {
+        return num1 + num2;
+    }
 }
