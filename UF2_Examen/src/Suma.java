b616e7e90dcc20897ccc77b7eb6cdfac9bad6bb7
Edgar Sanchez Gimenez <123939642+edsa292@users.noreply.github.com>
2023-05-12 11:22:59 +0200

    Update Suma.java

--- UF2_Examen/src/Suma.java
+++ UF2_Examen/src/Suma.java
@@ -1,7 +1,17 @@
 public class Suma {
     public int num1;
     public int num2;
-
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     // Mètode per retornar el num1
     public int getNum1() {
         return num1;
