e08fe7038031dd2d141e9d03953939fd16743cf1
Edgar Sanchez <edsa292@vidalibarraquer.net>
2023-05-12 11:16:57 +0200

    Push mètode getnum1

--- UF2_Examen/src/Suma.java
+++ UF2_Examen/src/Suma.java
@@ -2,8 +2,8 @@ public class Suma {
     public int num1;
     public int num2;
 
-    // Mètode per sumar dos números i retornar el resultat
-    public int sumar() {
-        return num1 + num2;
+    // Mètode per retornar el num1
+    public int getNum1() {
+        return num1;
     }
 }
